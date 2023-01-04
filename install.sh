23357f301b31f72c50329283e401a34a0653b149
adilimam <aimam1@gmail.com>
2023-01-04 02:15:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@ sudo cp -n /usr/share/X11/xkb/symbols/pc /usr/share/X11/xkb/symbols/pc.bck
 sudo cp pc_keyboard_mapping.txt /usr/share/X11/xkb/symbols/pc
 
 # Fix sound
-# OLD
-# from https://old.reddit.com/r/chrultrabook/comments/uc0b6i/howto_audio_on_apollolake_devices_under_linux/
-# sudo cp sof-apl-da7219.tplg /lib/firmware/intel/sof-tplg/
-# sudo cp inteldsp.conf /etc/modprobe.d/
-#
-# NEW
 git clone https://github.com/eupnea-linux/audio-scripts.git
 cd audio-scripts
 # Manual -- MODIFY setup-audio, comment out rmfile:
