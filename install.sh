8ba34c0c8aeaf540690133bc898d5cc4696174eb
adilimam <aimam1@gmail.com>
2023-01-04 23:02:19 -0500

    Explicit list of packages

--- install.sh
+++ install.sh
@@ -12,16 +12,48 @@ cd audio-scripts
 cd ..
 
 # Install necessary extensions
-sudo dnf install gnome-extensions-app gnome-shell-extension-dash-to-dock -y
-sudo dnf install gnome-tweaks -y
+sudo dnf install \
+  ffmpeg-free \
+  gnome-extensions-app \
+  gnome-shell-extension-dash-to-dock \
+  gnome-tweaks \
+  -y
 
-# Install the Fedora Educational Software group:
-sudo dnf group install --with-optional "Educational Software" -y
+# Install additional software
+sudo dnf install \
+  gcompris-qt \
+  scratch \
+  thonny \
+  pymol \
+  kalzium \
+  step \
+  kig \
+  kmplot \
+  kalgebra \
+  kbruch \
+  marble \
+  kgeography \
+  glglobe \
+  kstars \
+  stellarium \
+  gbrainy \
+  blinken \
+  kanagram \
+  klettres \
+  xboard \
+  tuxtype2 \
+  klavaro \
+  denemo \
+  vmpk \
+  rhythmbox \
+  dia \
+  gimp \
+  inkscape \
+  scribus \
+  vym \
+  -y
 
-# Install additional useful software:
-sudo dnf install ffmpeg-free gcompris-qt scratch thonny chemtool pymol dia gimp inkscape scribus klavaro vym wordnet audacity denemo atomix gbrainy -xboard -y
-
-# Remove packages that don't work well without internet access or with limited compute/storage, or are duplicitive
-sudo dnf remove gnome-boxes catgen gnome-photos kiten anki pairs ktouch fantasdic skychart corrida cantor parley kwordquiz moodle -y
+# Remove packages that don't work well without internet access or with limited compute/storage
+sudo dnf remove gnome-boxes gnome-photos -y
 
 # MANUAL: enable dash to dock, enable Applications menu, enable minimize and maximize buttons
