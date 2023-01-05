c77e307ea61972e29418293e20ef955cac324395
adilimam <aimam1@gmail.com>
2023-01-04 23:47:29 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,6 @@
 sudo cp -n /usr/share/X11/xkb/symbols/pc /usr/share/X11/xkb/symbols/pc.bck
 sudo cp pc_keyboard_mapping.txt /usr/share/X11/xkb/symbols/pc
 
-# Fix sound
-git clone https://github.com/eupnea-linux/audio-scripts.git
-
-echo "COMMENT-OUT the following line in audio-scripts/setup-audio:"
-echo '   rmfile("/lib/firmware/intel/avs/max98357a-tplg.bin")'
-echo "PRESS ENTER WHEN DONE"
-read
-
-cd audio-scripts
-./setup-audio
-cd ..
-
 # Install necessary extensions
 sudo dnf install \
   ffmpeg-free \
@@ -60,4 +48,14 @@ sudo dnf install \
 # Remove packages that don't work well without internet access or with limited compute/storage
 sudo dnf remove gnome-boxes gnome-photos -y
 
-# MANUAL: enable dash to dock, enable Applications menu, enable minimize and maximize buttons
+# Fix sound -- the system will reboot
+git clone https://github.com/eupnea-linux/audio-scripts.git
+
+echo "COMMENT-OUT the following line in audio-scripts/setup-audio:"
+echo '   rmfile("/lib/firmware/intel/avs/max98357a-tplg.bin")'
+echo "PRESS ENTER WHEN DONE. Select the avs driver at the next prompt."
+read
+
+cd audio-scripts
+./setup-audio
+cd ..
