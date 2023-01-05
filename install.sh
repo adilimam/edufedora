62f9fe4615de57fb00a5a336b4157e9d9f8f2104
adilimam <aimam1@gmail.com>
2023-01-05 18:13:58 -0500

    added vscode

--- install.sh
+++ install.sh
@@ -46,6 +46,12 @@ sudo dnf install \
   java-17-openjdk-devel \
   -y
 
+# vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code
+
 # Remove packages that don't work well without internet access or with limited compute/storage
 sudo dnf remove gnome-boxes gnome-photos -y
 
